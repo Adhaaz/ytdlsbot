0826ce21742ab853eeaaeeb1dd71b34d42fb9107
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-07-09 22:17:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ const config = require('./config');
 const app = express();
 const bot = new Telegraf(config.BOT_TOKEN);
 
-const downloadDir = path.join(__dirname, 'downloads');
-if (!fs.existsSync(downloadDir)) {
-    fs.mkdirSync(downloadDir);
-}
-
-const activeSessions = {};
-const ITEMS_PER_PAGE = 5;
-const MAX_FILE_SIZE_MB = 500;
-const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 require('./bot')(bot);
 
