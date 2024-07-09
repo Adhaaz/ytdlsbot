d20537b2bdad2b8a5520978886dde62edeadae3f
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-07-10 04:25:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+
+
+
+
+const express = require('express');
 const { Telegraf, Markup } = require('telegraf');
 const ytdl = require('ytdl-core');
 const fs = require('fs-extra');
 const path = require('path');
 const config = require('./config');
 
+const app = express();
 const bot = new Telegraf(config.BOT_TOKEN);
+
 
 require('./bot')(bot);
 
 bot.start((ctx) => ctx.reply('Selamat datang! Gunakan perintah /ytdl untuk mengunduh video atau audio dari YouTube.'));
 
+bot.telegram.setWebhook(`${config.URL}/bot${config.BOT_TOKEN}`);
+app.use(bot.webhookCallback(`/bot${config.BOT_TOKEN}`));
+
+app.get('/', (req, res) => {
+    res.send('Daflixx Bot is running!');
+});
+
 const PORT = process.env.PORT || 3000;
-bot.launch();
-console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
