f99e270b953820d427697e69206ffa760848c9e0
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-07-10 04:33:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     BOT_TOKEN: "7014683618:AAFPMWJCVI8ZuB2ruQxThtgD27CooZ7k8Rw",
-    URL: process.env.URL || 'https://ytdlsbot.netlify.app',
+    URL: process.env.URL || 'https://ytdlsbot.vercel.app',
 };
