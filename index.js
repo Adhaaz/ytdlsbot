b1acbc683ba02b4c3dd8ab5171b730b50fc154a5
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-07-14 08:37:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const { Telegraf, Markup } = require('telegraf');
 const express = require('express');
 const ytdl = require('ytdl-core');
@@ -7,22 +8,16 @@ const config = require('./config');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bot = new Telegraf(config.BOT_TOKEN);
 
+// Memulai bot Telegraf
+const bot = new Telegraf(config.BOT_TOKEN);
 require('./bot')(bot);
 
-bot.start((ctx) => ctx.reply('Selamat datang! Gunakan perintah /ytdl untuk mengunduh video atau audio dari YouTube.'));
+// Memulai server Express setelah bot dimulai
+bot.launch().then(() => {
+  console.log(`Bot running on ${config.BOT_TOKEN}`);
 
-app.use(bot.webhookCallback('/secret-path'));
-   bot.telegram.setWebhook(`https://female-crowd.surge.sh/secret-path`);
-
-   app.get('/', (req, res) => {
-     res.send('This is a Telegram bot server.');
-   });
-
-   app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}`);
-   });
-   
-bot.launch();
-console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
